0089dedd02630ce360eb22d3a166cdc484034e02
Yuri An <yuriandev@gmail.com>
2023-01-31 23:58:19 +0600

    feat: create an ID system

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,18 +1,30 @@
 import { Injectable } from '@angular/core';
 
+interface IModal {
+  id: string;
+  visible: boolean;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class ModalService {
-  private visible = false;
+  private modals: IModal[] = [];
 
   constructor() {}
 
+  register(id: string) {
+    this.modals.push({
+      id,
+      visible: false,
+    });
+  }
+
   isModalOpen() {
-    return this.visible;
+    return true;
   }
 
   toggleModal() {
-    this.visible = !this.visible;
+    //this.visible = !this.visible;
   }
 }
