0d01d1006ca73fb95ce235d563efd3819983bac2
Yuri An <yuriandev@gmail.com>
2023-02-12 13:41:35 +0600

    lesson 92: The ContentChildren Decorator

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ContentChildren, AfterContentInit } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
-  styleUrls: ['./tabs-container.component.css']
+  styleUrls: ['./tabs-container.component.css'],
 })
-export class TabsContainerComponent {
+export class TabsContainerComponent implements AfterContentInit {
+  @ContentChildren(TabComponent) tabs = {};
 
+  ngAfterContentInit(): void {}
 }
