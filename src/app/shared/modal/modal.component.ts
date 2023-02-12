e0370e45aedc5c79c175f3b86207cdbde18aa3e5
Yuri An <yuriandev@gmail.com>
2023-02-12 10:45:39 +0600

    lesson 90: CSS issues

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -7,11 +7,15 @@ import { ModalService } from 'src/app/services/modal.service';
   styleUrls: ['./modal.component.css'],
   //providers: [ModalService],
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
   @Input() modalID = '';
-  constructor(public modal: ModalService) {}
+  constructor(public modal: ModalService, public el: ElementRef) {}
 
   closeModal() {
     this.modal.toggleModal(this.modalID);
+  }
+
+  ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement);
   }
 }
