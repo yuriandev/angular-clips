d9bde7ba178a4bab345d40898186c854b5f6ba09
Yuri An <yuriandev@gmail.com>
2023-02-13 16:28:55 +0600

    lessons 93-97: working with tabs

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,4 +1,9 @@
-import { Component, ContentChildren, AfterContentInit } from '@angular/core';
+import {
+  Component,
+  ContentChildren,
+  AfterContentInit,
+  QueryList,
+} from '@angular/core';
 import { TabComponent } from '../tab/tab.component';
 
 @Component({
@@ -7,7 +12,24 @@ import { TabComponent } from '../tab/tab.component';
   styleUrls: ['./tabs-container.component.css'],
 })
 export class TabsContainerComponent implements AfterContentInit {
-  @ContentChildren(TabComponent) tabs = {};
+  @ContentChildren(TabComponent) tabs?: QueryList<TabComponent> =
+    new QueryList();
 
-  ngAfterContentInit(): void {}
+  ngAfterContentInit(): void {
+    const activeTabs = this.tabs?.filter((tab) => tab.active);
+
+    if (!activeTabs || activeTabs.length === 0) {
+      this.selectTab(this.tabs!.first);
+    }
+  }
+
+  selectTab(tab: TabComponent) {
+    this.tabs?.forEach((tab) => {
+      tab.active = false;
+    });
+
+    tab.active = true;
+
+    return false;
+  }
 }
