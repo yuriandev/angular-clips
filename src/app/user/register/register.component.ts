88bb1a44ee0b65a7c792b2fa25b9d9d56cd78d39
Yuri An <yuriandev@gmail.com>
2023-05-28 23:45:11 +0600

    Lesson 109: Updating the Template

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -7,7 +7,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  name = new FormControl('', [Validators.required]);
+  name = new FormControl('', [Validators.required, Validators.minLength(3)]);
   email = new FormControl('');
   age = new FormControl('');
   password = new FormControl('');
